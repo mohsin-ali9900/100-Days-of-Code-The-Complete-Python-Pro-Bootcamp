69a5c6d95fc10b1f1f5829facbdf80718dcfdbc4
Mohsin Ali <120159587+mohsin-ali9900@users.noreply.github.com>
2025-08-27 22:43:11 +0500

    Add files via upload

--- SecretAuctionProgram/main.py
+++ SecretAuctionProgram/main.py
@@ -1,1 +1,21 @@
+import os
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
+print("Welcome to secret Auction Program")
+more = "yes"
+bids = {}
+while more == "yes":
+    name = input("What is your name?: ")
+    bid = int(input("What is your bid?: $"))
+    bids[name] = bid
+    more = input("Are there any other bidders? Type 'yes' or 'no'.\n").lower()
+    clear_screen()
+
+highest_bid = 0
+winnerBidder = ""
+for i in bids:
+    if bids[i] > highest_bid:
+        highest_bid = bids[i]
+        winnerBidder = i
+print(f"Winner is {winnerBidder} and the winning bid amount is {highest_bid}")
